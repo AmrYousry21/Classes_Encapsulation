14d1d6dbc707fe2f1e027b5dfc376f2c5b7483b8
Amr Yousry <amryousry19@gmail.com>
2022-08-19 11:45:05 -0400

    Classes Exercise One

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,19 @@
+﻿namespace BankEncapsulation
+{
+    public class BankAccount 
+    {
+        private double _balance = 0.0;
+
+        public void Deposit (double num) 
+        {
+            this._balance += num;
+           
+        }
+
+        public void GetBalance() 
+        {
+            Console.WriteLine($"Your balance is: {_balance}");
+        }
+    
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+
+            Console.WriteLine("Enter ammount you wish to deposit:");
+            double input = double.Parse(Console.ReadLine());
+
+            while (input != 0)
+            {
+
+                account.Deposit(input);
+
+                account.GetBalance();
+
+                Console.WriteLine("Enter ammount you wish to deposit or Enter 0 to Exit:");
+                input = double.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
